f36fa3e65d5b4fba181e001bf13af98ae3cdf54b
c11tadeL <119813015+c11tadeL@users.noreply.github.com>
2023-04-28 23:10:46 +0300

    add block1_task8(Popilevych)

---
+++ lb4_first_block/Popilevych.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lb4_popilevych_block1
+{
+    public partial class Popilevych 
+    {
+        public static void DeleteOddElem(ref int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            int cnt = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i%2!=0)
+                {
+                    newArray[cnt] = array[i];
+                    cnt++;
+                }
+            }
+            Array.Resize(ref newArray, cnt);
+            array = newArray;
+        }
+    }
+}
